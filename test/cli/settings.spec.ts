1814cc174d719866e6253d6feeff8e93abed4aa5
xingxingmofashu <xingxingmofashu@outlook.com>
2025-06-23 13:46:07 +0800

    chore: add `vitest` test

---
+++ test/cli/settings.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it } from "vitest";
+import { initSettings } from "../../packages/cli/src";
+
+describe('Init Settings', () => {
+  
+  it('generate winget desktop settings', async () => {
+    await initSettings()
+  })
+
+})
